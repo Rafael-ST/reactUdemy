424060e22666abaf03e34a01f1de312808dbe908
Rafael Santos <rafaelsantost87@gmail.com>
2024-07-30 16:57:30 -0300

    continue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,16 @@ import {View, Text, StyleSheet} from 'react-native'
 // import {Comp, Comp01} from '../components/Multi'
 // import Primeiro from '../components/primeiro'
 // import Estilo from '../components/estilo'
-import MinMax from '../components/MinMax'
+// import Aleatorio from '../components/Aleatorio'
+// import MinMax from '../components/MinMax'
+import Titulo from '../components/Titulo'
 
 export default () => (
   <View style={style.App}>
-    <MinMax min="3" max="20"/>
-    {/* <Comp />
+    <Titulo principal="Cadastro Produto" secundario="Tela de Cadastro do Produto"/>
+    {/*<MinMax min="3" max="20"/>
+    <Aleatorio min={1} max={60} />
+     <Comp />
     <Primeiro/>
     <Comp01/> */}
   </View>
